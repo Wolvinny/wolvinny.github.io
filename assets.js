2dd59580133676a2b8fdc3fd1385ad5985ce16ef
Wolvinny <treblecorgi@gmail.com>
2022-02-21 09:26:21 +0100

    icontest

--- assets.js
+++ assets.js
@@ -1,3 +1,7 @@
 document.getElementById("rickroll").onclick = function(){
     location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 };
+
+document.getElementById("site").onclick = function(){
+    location.href = ""
+};
